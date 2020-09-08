edd2893051cf779a8e444dac75ed69be6dd42503
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-09-07 21:08:15 -0400

    add sprint challenge

--- src/sprint.py
+++ src/sprint.py
@@ -1,4 +1,15 @@
-import math
-primes = [math.floor(x / 2)  for x in range(30) ]
+def prime_numbers(number):
+    total = []
+    for i in range (2,  number + 1):
+        if(i % 2 != 0 or i == 2):
+            total.append(i)
+    return total
 
-print(primes)
+def is_prime(number):
+    primes = prime_numbers(number)
+    if number in primes:
+        print(f'{number} is a prime!')
+    else:
+        print(f'{number} is not a prime!')
+
+is_prime(200)
